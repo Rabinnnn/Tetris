2528f493535c8d1ec13cf1a19de3dc163b44577e
rotieno <rabinyitzahck@gmail.com>
2024-12-05 10:29:33 +0300

    feat: implement function that creates a board

---
+++ create_board.go
@@ -0,0 +1,13 @@
+package utils
+//CreateBoard generated a 2D slice representing a board.
+//Each cell is initialized with the string "."
+func CreateBoard(size int) [][]string{
+	board := make([][]string, size)
+	for i := range board{
+		board[i] = make([]string, size)
+		for j := range board[i]{
+			board[i][j] = "."
+		}
+	} 
+	return board
+}
