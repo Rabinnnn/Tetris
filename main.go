65cf8b570419f03c1d5b4056e1d663877f287256
rotieno <rabinyitzahck@gmail.com>
2024-12-09 13:45:21 +0300

    feat: solve and display the solved board

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+import(
+	"fmt"
+	"math"
+	"tetris/utils"
+)
+
+func main(){
+	tetrominoes := utils.Reader()
+	
+	output := utils.IsValid(tetrominoes)
+	if output == "ERROR"{
+		fmt.Println("ERROR")
+		return
+	}
+
+	tetrominoes = utils.Trimmer(tetrominoes)
+
+	size := int(math.Ceil(math.Sqrt(float64(len(tetrominoes) * 4))))
+
+	var solvedBoard [][]string
+	for {
+		board := utils.CreateBoard(size)
+		solvedBoard = utils.SolveBoard(board, tetrominoes)
+		if solvedBoard != nil{
+			break
+		}
+		size++
+	}
+	utils.PrintBoard(solvedBoard)
+}
