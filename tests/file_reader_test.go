c21beb709d9da3a4e02792078bf79cd270b7f5e7
rotieno <rabinyitzahck@gmail.com>
2024-12-11 12:11:01 +0300

    test: add test for Reader function

---
+++ tests/file_reader_test.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"reflect"
+	"testing"
+	"tetris/utils"
+)
+
+func TestReader(t *testing.T) {
+	tests := []struct {
+		name string
+		want [][]string
+	}{
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := utils.Reader(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Reader() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
